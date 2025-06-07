542663ccab35746aafa9f0f89187a80950b94409
Marckxa <mafajul@gmail.com>
2025-06-07 18:48:38 +0200

    Update Sum.py

---
+++ entities/functions/Sum.py
@@ -0,0 +1,13 @@
+from typing import List , Union
+from Argument import Argument
+from Function import Function
+from NumericValue import NumericValue
+
+
+# Definimos un alias para tipos numéricos
+Number = Union[int, float]
+
+class SUM(Function):
+    def compute_formula(self, arguments: List[Argument]) -> Number:
+        self.result = sum(arg.get_value() for arg in arguments)
+        return self.result
