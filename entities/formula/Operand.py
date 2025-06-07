1bcafcdd4a0268f7f0961099382639a3622ffca2
Marckxa <mafajul@gmail.com>
2025-06-07 19:24:36 +0200

    Update Operand.py

--- entities/formula/Operand.py
+++ entities/formula/Operand.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 
 class Operand(ABC):
+    def __init__(self, operator: str) -> None:
+    self.operator: str = operator
+
     @abstractmethod
     def get_operand(self) -> object:
         pass
