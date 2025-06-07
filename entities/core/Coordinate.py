77bdb5316208d03844219ba4d620b3b6766f141f
Marckxa <mafajul@gmail.com>
2025-06-07 19:55:50 +0200

    CellRange Update

--- entities/core/Coordinate.py
+++ entities/core/Coordinate.py
@@ -8,10 +8,3 @@ class Coordinate:
 
     def __str__(self):
         return f"{self.column_id}{self.row_id}"
-
-    @staticmethod
-    def from_string(coord_str: str) -> 'Coordinate':
-        match = re.match(r"([A-Z]+)(\d+)", coord_str.upper())
-        if not match:
-            raise ValueError(f"Invalid coordinate format: {coord_str}")
-        return Coordinate(match[1], int(match[2]))
